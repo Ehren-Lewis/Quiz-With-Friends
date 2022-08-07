80f3eb3ee44b8da3d99d67c73abd096a3cb9a8a8
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-07 16:59:06 -0500

    Tried figuring out a way to have the javascript engine stop but still run while waiting for a button press but I haven't found a way to do so synchronously yet. I will attempt later by not using a for loop as well as not using a separate callback function but instead an arrow function entriely within the addEventLIstener so the variables can be local and I won't have to worry about trying to have a reutrn type for an eventListener and not a typical function call

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -13,15 +13,38 @@ const question1 = {
     "Answer": "Document Object Model"
 }
 
+const question2 = {
+    "What does HTML stand for?": [
+        "HyperText Markdown Language",
+        "High Text Markup Logs",
+        "HyperText Markup Language",
+        "HyperText Markdown Logs"
+    ],
+    "Answer": "HyperTExt Markup Language"
+}
 
+let globalScore = 0;
 
+// right answer will give 10 points 
 
 
 // Add more questions later
 const questionArray = [question1];
 
 
+const questionButtonHandler = (targetButton, answer, questionList) => {
 
+    if (targetButton.innerHTML == answer) {
+        globalScore += 10;
+        while (questionList.hasChildNodes()) {
+            questionList.removeChild(questionList.children[0]);
+        }
+    }
+
+
+
+
+}
 
 const startQuiz = (questioneEle, AnswerEle) => {
     const questionBox = document.querySelector(".question-container");
@@ -39,19 +62,28 @@ const startQuiz = (questioneEle, AnswerEle) => {
         const questionList = document.querySelector(".question-list");
         console.log(questionList);
 
+        const answerButtonsList = [];
+
         for( let j = 0; j < selection.length; j++) {
             const buttonAnswers = document.createElement('button');
             buttonAnswers.setAttribute('text', selection[j]);
             buttonAnswers.innerHTML = selection[j];
-
-            buttonAnswers.addEventListener('click', () => {
-                console.log(buttonAnswers.innerHTML);
-            })
-
-            questionList.appendChild(buttonAnswers);
+            answerButtonsList.push(buttonAnswers);
         }
+            // buttonAnswers.addEventListener('click',  () => {
+            //     questionButtonHandler(buttonAnswers, answer);
+            // ;
 
 
+        for (let i = 0; i < answerButtonsList.length; i ++) {
+            answerButtonsList[i].addEventListener('click', () => {
+                questionButtonHandler(answerButtonsList[i], answer, questionList);
+
+            });
+            questionList.appendChild(answerButtonsList[i]);
+
+            while (questionList.hasChildNodes()) {continue};
+        }
     }
 }
 
@@ -60,13 +92,11 @@ const hideElements = () => {
     quizIntro.setAttribute("style", "display: none");
     startButton.setAttribute("style", "display: none");
     startQuiz();
-
-
 }
 
 
 
-startButton.addEventListener('click', hideElements)
+startButton.addEventListener('click', hideElements);
 
 
 // Test psuedo code
