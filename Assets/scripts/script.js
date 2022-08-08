fd5014cced463712b48d1ecdec01a91c66754595
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-08 14:13:56 -0500

    Got it to where all the child nodes of the ul are removed per question, the score is also updated per question, the only thing is to handle the end of the quiz, as well as saving scores, and handling timeout

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -20,17 +20,19 @@ const question2 = {
         "HyperText Markup Language",
         "HyperText Markdown Logs"
     ],
-    "Answer": "HyperTExt Markup Language"
+    "Answer": "HyperText Markup Language"
 }
 
+let globalCount = 0;
 let globalScore = 0;
 
 // right answer will give 10 points 
 
 
 // Add more questions later
-const questionArray = [question1];
+const questionArray = [question1, question2];
 
+const maxCount = questionArray.length;
 
 const postQuizScreen = (score) => {
     console.log(score);
@@ -38,7 +40,8 @@ const postQuizScreen = (score) => {
 
 
 const setTimer = (time) => {
-    startQuiz();
+    // startQuiz();
+    newQuiz(questionArray, globalCount);
 
     var timerElement = setInterval( () => {
 
@@ -54,55 +57,85 @@ const setTimer = (time) => {
             box.setAttribute("style", "display: none");
         }
 
-    }, 250)
+    }, 1000);
 }
 
-
-
-
-
-
-const questionButtonHandler = (targetButton, answer) => {
+const questionButtonHandler = (targetButton, answer, questionList) => {
     if (targetButton.innerHTML == answer) {
         globalScore += 10;
         console.log(globalScore);
     }
+    while (questionList.children.length > 0) {
+        questionList.removeChild(questionList.firstChild);
+    }
+
+    globalCount++;
+    newQuiz(questionArray, globalCount);
 }
 
-const startQuiz = (questioneEle, AnswerEle) => {
+// const startQuiz = (questioneEle, AnswerEle) => {
+//     const questionBox = document.querySelector(".question-container");
+//     questionBox.setAttribute("style", "display: block");
+//     for (let quizItem of questionArray) {
+//         // The actualy question for each question
+//         const question = Object.keys(quizItem)[0];
+//         const answer = Object.entries(quizItem)[1][1];
+//         console.log(answer);
+//         //  This is an array.
+//         const selection = Object.entries(quizItem)[0][1];
+
+//         document.querySelector(".question-title").innerHTML = question;
+
+//         const questionList = document.querySelector(".question-list");
+//         console.log(questionList);
+
+//         const answerButtonsList = [];
+
+//         for( let j = 0; j < selection.length; j++) {
+//             const buttonAnswers = document.createElement('button');
+//             buttonAnswers.setAttribute('text', selection[j]);
+//             buttonAnswers.innerHTML = selection[j];
+//             answerButtonsList.push(buttonAnswers);
+//         }
+
+
+//         for (let i = 0; i < answerButtonsList.length; i ++) {
+//             answerButtonsList[i].addEventListener('click', () => {
+//                 questionButtonHandler(answerButtonsList[i], answer);
+//             });
+//             questionList.appendChild(answerButtonsList[i]);
+//         }
+//     }
+// }
+
+const newQuiz  = (questions, count) => {
+
     const questionBox = document.querySelector(".question-container");
     questionBox.setAttribute("style", "display: block");
-    for (let quizItem of questionArray) {
-        // The actualy question for each question
-        const question = Object.keys(quizItem)[0];
-        const answer = Object.entries(quizItem)[1][1];
-        console.log(answer);
-        //  This is an array.
-        const selection = Object.entries(quizItem)[0][1];
-
-        document.querySelector(".question-title").innerHTML = question;
-
-        const questionList = document.querySelector(".question-list");
-        console.log(questionList);
-
-        const answerButtonsList = [];
-
-        for( let j = 0; j < selection.length; j++) {
-            const buttonAnswers = document.createElement('button');
-            buttonAnswers.setAttribute('text', selection[j]);
-            buttonAnswers.innerHTML = selection[j];
-            answerButtonsList.push(buttonAnswers);
-        }
-
-
-        for (let i = 0; i < answerButtonsList.length; i ++) {
-            answerButtonsList[i].addEventListener('click', () => {
-                questionButtonHandler(answerButtonsList[i], answer);
-            });
-            questionList.appendChild(answerButtonsList[i]);
-
-        }
+    if (count == maxCount) {
+        console.log('done');
+        // questionBox.setAttribute("style", "display: none");
+        // return;
     }
+    const currentQuestion = Object.entries(questions[count])[0][0];
+    const currentResponses = Object.entries(questions[count])[0][1];
+    const answer = Object.entries(questions[count])[1][1];
+
+    const questionEle = document.querySelector(".question-title");
+    questionEle.textContent = currentQuestion;
+
+    const questionList = document.querySelector(".question-list");
+    for (let i = 0; i < currentResponses.length; i++) {
+            const buttonAnswers = document.createElement('button');
+            buttonAnswers.textContent = currentResponses[i];
+            buttonAnswers.addEventListener('click', () => {
+                questionButtonHandler(buttonAnswers, answer, questionList);
+            });
+            questionList.appendChild(buttonAnswers);
+    }
+
+    const questionNodes = questionList.childNodes;
+
 }
 
 
@@ -112,10 +145,7 @@ const hideElements = () => {
     setTimer(time);
 }
 
-
-
 startButton.addEventListener('click', hideElements);
-
 
 // Test psuedo code
 // Have there be a question container with display none
@@ -124,9 +154,6 @@ startButton.addEventListener('click', hideElements);
 // Each question will be a button wrapped in a list
 // On click, check the value of the button 
 // if that value == question answer, they get more points
-
-
-
 
 // timeout function
 // what to do when timeout function hits 0? 
